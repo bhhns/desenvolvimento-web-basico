57ee0feac265a4e5ed68b55721fadd8b87e042ee
bhhns <brunohns13@gmail.com>
2025-10-30 21:37:33 -0300

    aprendendo JS

---
+++ AULA 9/index.js
@@ -0,0 +1,8 @@
+function Ligar() {
+    document.getElementById('lampada').src = 'Captura de tela 2025-10-30 210605.png';
+}
+
+function Desligar() {
+    document.getElementById('lampada').src = 'Captura de tela 2025-10-30 210553.png';
+}
+
